0494f1e559bd2dcec6b9cfcf171516788efd6754
davi <davi@10.0.0.20>
2014-05-22 16:22:58 +0200

    Modificati base url in config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://10.0.0.213/ci/';
+$config['base_url']	= 'http://localhost/flypizza/';
 
 /*
 |--------------------------------------------------------------------------
