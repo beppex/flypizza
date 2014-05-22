fc118a1558b936cba20050adfcbc1dba5fec32c5
davi <davi@10.0.0.20>
2014-05-22 15:58:03 +0200

    Revert "Modifica Prova"
    This reverts commit 84713142f96ffb92a34fcff004254494350bc309.

--- application/controllers/administrator.php
+++ application/controllers/administrator.php
@@ -13,10 +13,6 @@
  */
 class Administrator extends CI_Controller
 {
-    public function __construct() {
-        parent::__construct();
-    }
-
     public function index()
     {
         $this->login();
